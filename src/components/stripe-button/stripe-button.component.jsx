81d7096b0d1e550c3081401e15c61844dc0ad3fc
IzzHrz <izzharizdev@gmail.com>
2020-10-16 18:54:22 +0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HcpYmKP6UE3xOsRwpDqTh6YjMwtzuG0OXcI42n7oitI9zaePV1mIv6IE05rZU3gI8HmcscDS4nvsCkx24VK6kNB00KXccsnid'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return(
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton
