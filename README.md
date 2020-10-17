a1253d79c228bd970bba17a20f54167beead087e
IzzHrz <izzharizdev@gmail.com>
2020-10-17 19:07:54 +0800

    changed checkout naming and added readme info

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # HRZ Clothing - E-commerce site built with React
 
 - Initialized project with create-react-app.
+- Used redux state management to reuse components in the cart component.
 - Firebase for signup / signin auth including google login.
 - Integrated Stripe API for payment handling.
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -13,7 +13,7 @@ const StripeCheckoutButton = ({ price }) => {
     return(
         <StripeCheckout
             label='Pay Now'
-            name='CRWN Clothing Ltd.'
+            name='Hrz Clothing Ltd.'
             billingAddress
             shippingAddress
             image='https://sendeyo.com/up/d/f3eb2117da'
